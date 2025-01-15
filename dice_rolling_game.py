ad1eed2bfee4c5fc8f4c54caa8ce13d03552134d
Beth-H <bethhughesis@hotmail.com>
2025-01-15 01:19:06 +0000

    add first commit in python

---
+++ dice_rolling_game.py
@@ -0,0 +1,8 @@
+
+# Loop
+# Question: roll the dice? 
+# If user enters y - generate and print 2 random nums 
+# If user enters n - print 'Thanks for playing!' and terminate program
+# Else print invalid choice
+
+input("Roll the dice? (y/n)")
